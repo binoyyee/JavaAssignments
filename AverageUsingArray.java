2fcb454d9ac21b4af9b166b6a489571a595eabb7
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 11:31:29 +0530

    Create AverageUsingArray.java

---
+++ AverageUsingArray.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Check whether a string is palindrome or not
+        double[] arr = {19, 12.89, 16.5, 200, 13.7};
+        double total = 0;
+
+        for(int i=0; i<arr.length; i++){
+            total = total + arr[i];
+        }
+
+                double average = total / arr.length;
+                System.out.format("The average is: %.3f", average); /* %.3f used to display the formatted output after decimal point */
+            }
+        }
