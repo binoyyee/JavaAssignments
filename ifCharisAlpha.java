4a5aba149b35b70232f9a4cdda4f4e715a2af2d2
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 14:52:02 +0530

    Create ifCharisAlpha.java

---
+++ ifCharisAlpha.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.*;
+import java.util.Arrays;
+import java.util.Map.Entry;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+                       // check if character is alphabet
+                char c = '*';
+
+                if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                    System.out.println(c + " is an alphabet.");
+                else
+                    System.out.println(c + " is not an alphabet.");
+            }
+        }
