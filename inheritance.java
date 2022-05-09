13aa8aaf917a62032bd69ef57e2d5025aef6013f
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 16:03:36 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,30 @@
+class Animal1
+{  
+void eat()
+{
+	System.out.println("eating...");}  
+}  
+class Dog extends Animal1
+{  
+void bark()
+{
+	System.out.println("barking...");}  
+}  
+
+class BabyDog extends Dog
+{  
+void weep()
+{
+	System.out.println("weeping...");}  
+}  
+public class Inheritance {
+
+	public static void main(String[] args) {
+		BabyDog d=new BabyDog();  
+		d.weep();  
+		d.bark();  
+		d.eat();  
+
+	}
+
+}
