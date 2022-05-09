384103d46a75819125bfcfdc1a5b990f83c31071
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 11:25:29 +0530

    Create PalindromeCheck.java

---
+++ PalindromeCheck.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Check whether a string is palindrome or not
+        String a, b = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the string you want to check:");
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("The string is palindrome.");
+        }
+        else
+        {
+            System.out.println("The string is not palindrome.");
+        }
+    }
+}
