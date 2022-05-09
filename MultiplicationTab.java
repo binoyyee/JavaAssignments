2465ee4ffc959c576d93e4d82ff546b57fa3ff97
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 14:55:52 +0530

    Create MultiplicationTab.java

---
+++ MultiplicationTab.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.*;
+import java.util.Arrays;
+import java.util.Map.Entry;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+                       // check if character is alphabet
+      int num = 5;
+                for(int i = 1; i <= 10; ++i)
+                {
+                    System.out.printf("%d * %d = %d \n", num, i, num * i);
+                }
+            }
+        }
