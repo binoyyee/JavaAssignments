ef7449fa070ede1d6eb9b9e91a577428280f2296
Binoy <44939841+binoyyee@users.noreply.github.com>
2022-05-09 15:00:05 +0530

    Create Alphabets.java

---
+++ Alphabets.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.*;
+import java.util.Arrays;
+import java.util.Map.Entry;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+                       // check if character is alphabet
+                char c;
+                for(c = 'A'; c <= 'Z'; ++c)
+                    System.out.print(c + " ");
+            }
+        }
